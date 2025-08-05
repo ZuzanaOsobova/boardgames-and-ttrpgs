6fd6efc156b4e4ce81e2b53d486001443f68f560
Zuzana Osobová <osoz00@vse.cz>
2025-08-05 21:33:24 +0200

    JS test - funkční JSON objekt propisující se innerHTML ukázka, jak vytvořit img

--- test.js
+++ test.js
@@ -1,15 +1,38 @@
-fetch('sample.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        document.getElementById('title').textContent = data.title;
-        document.getElementById('message').textContent = data.message;
-    })
-    .catch(error => {
-        console.error('Error loading JSON:', error);
-    });
+let boardgames = [];
 
+//funkce načítající json do proměné boardgames
+async function loadBoardgames (){
+    try {
+        const response = await fetch('boardgames.json');
+        if(!response.ok){
+            throw new Error('Network response was not ok.')
+        }
+
+        boardgames = await response.json();
+
+        // Tady voláme jinou funkci, která ve skutečnosti ukáže json v html
+        displayHTML();
+    } catch (error){
+        console.log("Error loading JSON: " + error)
+    }
+}
+
+function displayHTML() {
+
+    const list = document.getElementById('game');
+
+    boardgames.forEach( function (game){
+
+        //Mini kontrola, že je json objekt vyplněný, protože tam mám automaticky nevyplněné
+        if(game.name){
+            const item = document.createElement('li');
+
+            item.innerHTML = "<img src='images/" + game.image + "'>"
+            list.appendChild(item)
+        }
+
+    })
+
+}
+
+loadBoardgames()
